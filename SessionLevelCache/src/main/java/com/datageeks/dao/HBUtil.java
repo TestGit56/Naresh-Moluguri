67e78fbe984f83e988f3ac660378d2bd799c1367
unknown <TestGit56@gmail.com>
2019-09-05 13:45:00 +0530

    Session Level Cache

---
+++ SessionLevelCache/src/main/java/com/datageeks/dao/HBUtil.java
@@ -0,0 +1,26 @@
+package com.datageeks.dao;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HBUtil {
+
+ public static final SessionFactory SESSION_FACTORY;
+
+ static
+ {
+	 synchronized(HBUtil.class)
+	 {
+		 Configuration cfg = new Configuration();
+		 cfg.configure("oracle.cfg.xml");
+		 SESSION_FACTORY = cfg.buildSessionFactory();
+	 }
+ }
+
+	public static SessionFactory getSessionFactory()
+	{
+		return SESSION_FACTORY;
+	}
+}
+ 
+
